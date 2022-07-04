0c0a20810b53e2edce41ba46effc3256a270579f
yoonjin-Jung <102291480+D0ngHyeon@users.noreply.github.com>
2022-07-04 12:06:07 +0900

    test golf swing anim

--- MAIN/JS/controls.js
+++ MAIN/JS/controls.js
@@ -1,3 +1,20 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
 function play(){
   const modelViewer = document.querySelector("#barbell_squat");
   //modelViewer.animationName='barbell_squat_anim';
@@ -15,5 +32,9 @@ function stop(){
   modelViewer.pause();
 }
 function loadbarbellsquat(){
-  //const 
+  const modelViewer = document.querySelector("#barbell_squat") // 
 }
+$( "slide" ).click(function() {
+  $( this ).empty();
+});
+
